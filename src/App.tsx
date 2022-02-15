d7121a347581e8971af8ebfb052252dc6be04ef7
hexcowboy <accounts@cowboy.dev>
2022-02-15 14:24:21 -0700

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import { Provider } from "react-redux";
 import Home from "./routes/home";
 import Explore from "./routes/explore";
+import Create from "./routes/create";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TabBar from "./components/TabBar";
-import store from './redux/store';
-
+import store from "./redux/store";
 
 function App() {
-  
-
   return (
     <div>
       <Provider store={store}>
         <BrowserRouter>
-            <TabBar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="explore" element={<Explore />} />
-              </Routes>
+          <TabBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="create" element={<Create />} />
+          </Routes>
         </BrowserRouter>
       </Provider>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,23 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import Web3 from 'web3'
+import Web3 from "web3";
 // import { Web3ReactProvider } from '@web3-react/core'
 // import { MetaMaskProvider } from './hooks/useMetaMask'
 
-// function getLibrary(provider, connector) { 
+// function getLibrary(provider, connector) {
 //   return new Web3(provider)
 // }
 
 ReactDOM.render(
   // <Web3ReactProvider getLibrary={getLibrary}>
   //   <MetaMaskProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
   //   </MetaMaskProvider>
   // </Web3ReactProvider>
-  ,document.getElementById("root")
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
