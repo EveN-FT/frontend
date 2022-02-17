a4fb0b5e1dba68e849f733656a4fafe8ceaf89e9
hexcowboy <accounts@cowboy.dev>
2022-02-17 11:37:20 -0700

    change links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import ConnectInjected from "./wallet/ConnectInjected";
 import ConnectWalletConnect from "./wallet/ConnectWalletConnect";
 import ConnectCoinbase from "./wallet/ConnectCoinbase";
@@ -14,7 +15,9 @@ const NavBar = () => {
       <div className="navbar">
         <ul>
           <li>
-            <img src={ticketIcon} alt="Ticket icon" className="icon" />
+            <Link to="/">
+              <img src={ticketIcon} alt="Ticket icon" className="icon" />
+            </Link>
           </li>
           <li>
             <button onClick={() => setOpenModal(true)}>Connect</button>
