a7513f7144cea41471e135649c3d5e30425a08f2
Bradley <bxs388@gmail.com>
2022-02-18 13:09:16 -0700

    api call for wallet

--- src/routes/Wallet.tsx
+++ src/routes/Wallet.tsx
@@ -1,28 +1,31 @@
 import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import "../styles/wallet.scss";
-import ticketDesign from "../assets/03-ticketdesign.jpeg";
+import { useWeb3React } from "@web3-react/core";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Wallet = () => {
+    const COVALENT_API_BASE = `https://api.covalenthq.com/v1`
+    const COVALENT_KEY = process.env.REACT_APP_COVALENT_KEY!
+    const CHAIN_ID = 1
+    const { active, account, activate, deactivate } = useWeb3React()
 
     //COVALENT API CALL get all tokens belonging to signed in wallet
-    const userTickers = [
-        {
-          title: "Deadmau5",
-          description:
-            "Deadmau5 is playing his 4 millionth show at the Temple Theater in downtown Denver. Wednesday, February 16th from 5:00pm - 2:00am",
-          imageUrl: "https://",
-          contractId: "0xddd1d123e53a1acf61a47ba592e62b240199b1a6",
-        },
-        {
-          title: "Chainlink Happy Hour",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          imageUrl: "https://",
-          contractId: "0xddd1d123e53a1acf61a47ba592e62b240199b1a6",
-        },
-      ];
+    console.log('User logged in as ',account)
+    const [tokens, setTokens] = useState([]);
+    useEffect(() => {
+        const COVALENT_GET_TOKENS = `${COVALENT_API_BASE}/${CHAIN_ID}/${account}/balances_v2/?quote-currency=USD&format=JSON&nft=true&no-nft-fetch=false&key=${process.env.REACT_APP_COVALENT_KEY}`
+        axios.get(COVALENT_GET_TOKENS).then(({ data }) => {
+            setTokens(data);
+            console.log('api return',data)
+        }).catch(console.error)
+    }, []);
 
+
+    //TODO add logout option here with 'deactive' so it can be removed from navbar's walletconnection component
+    //TODO filter tokens for just ours, then map
+    //TODO link to event detail page for each token when selected
 
   return (
     <>
