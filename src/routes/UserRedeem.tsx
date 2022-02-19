08878c90f6c9fb571e01fabd8f6f13ef1910cdf9
Mubashir Mir <mubashirmir15@gmail.com>
2022-02-19 15:24:38 -0700

    get signing and verifying to work:

--- src/routes/UserRedeem.tsx
+++ src/routes/UserRedeem.tsx
@@ -1,12 +1,77 @@
 import { useState } from 'react';
 import {TextField, TextareaAutosize, Grid} from '@material-ui/core'
 import QRcode from 'qrcode.react';
+import { useWeb3React } from '@web3-react/core';
+import { ethers } from 'ethers';
 
 const UserRedeem = () => {
   const [qr, setQr] = useState('Tiny Tix');
   const handleChange = (event:any) => {
     setQr(event.target.value);
   };
+  const { account, library, chainId } = useWeb3React();
+  const web3 = useWeb3React();
+  const [message, setMessage] = useState("test");
+  const [signedMessage, setSignedMessage] = useState("");
+
+  const sign = async () => {
+    const domain = {
+      name: "Tiny Tix",
+      version: "1",
+      chainId: chainId,
+      verifyingContract: "0xb628a2038Bb4BF52486ffDD8dd2Eb07e73ddA5dA",
+    };
+
+    // The named list of all type definitions
+    const types = {
+      Event: [
+        { name: "Name", type: "string" },
+        { name: "Address", type: "address" },
+      ],
+      Ticket: [
+        { name: "Event", type: "Event" },
+        { name: "Owner", type: "address" },
+        { name: "ID", type: "uint256" },
+      ],
+    };
+
+    // The data to sign
+    const value = {
+      Event: {
+        Name: "Deadmau5",
+        Address: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+      },
+      Owner: account,
+      ID: 2,
+    };
+    const signer = await library.getSigner();
+    const signedMessage = await signer._signTypedData(domain, types, value);
+    setSignedMessage(signedMessage);
+    console.log(signedMessage);
+    setQr(JSON.stringify({
+      "domain": domain,
+      "types": types,
+      "value": value,
+      "signedMessage": signedMessage
+    }))
+  };
+
+  const verify = async () => {
+    const qrInfo = JSON.parse(qr)
+    const verified = ethers.utils.verifyTypedData(
+      qrInfo.domain,
+      qrInfo.types,
+      qrInfo.value,
+      qrInfo.signedMessage
+    );
+    console.log(verified);
+    // get owneraddres qrInfo.value.ID ownerOf(tokenId)
+    if (verified === qrInfo.value.ID) {
+      console.log("verified")
+    }
+  };
+  // we still to grab ticket data
+  // ticket contract address, ticket id, owner address
 
   return (
     <div>
@@ -43,6 +108,8 @@ const UserRedeem = () => {
                 </Grid> : ''
               }
             </div>
+            <button onClick={sign}>Sign</button>
+            <button onClick={verify}>Verify</button>
       </div>
 
   );
