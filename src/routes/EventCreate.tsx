a2d8499b0bf4ca64bf548229f25d5352ced99120
hexcowboy <accounts@cowboy.dev>
2022-02-19 16:42:37 -0700

    upload image and metadata to ipfs

--- src/routes/EventCreate.tsx
+++ src/routes/EventCreate.tsx
@@ -1,22 +1,89 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useState, FormEventHandler, useEffect } from "react";
-import "../styles/event-create.scss";
-
+import { create, CID } from "ipfs-http-client";
 import { getWeb3ReactContext, useWeb3React } from "@web3-react/core";
 import { ethers } from "ethers";
 
+import "../styles/event-create.scss";
+
 const EventCreate = () => {
+  const [image, setImage] = useState<FileList | null>(null);
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [venue, setVenue] = useState("");
+  const [city, setCity] = useState("");
   const [datetime, setDatetime] = useState(new Date().toISOString());
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const [bc, setbc] = useState<string>();
 
-  const handleSubmit = (event: React.SyntheticEvent) => {
+  const handleSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
+
     // ipfs upload
+    if (image === null) {
+      // handle no image here
+      console.log("no image");
+      return;
+    }
+
+    // Read the file uploaded to get image dimensions and type
+    var imageData = new Image();
+    var imageType = "";
+    var fileReader = new FileReader();
+
+    fileReader.onload = async () => {
+      imageData.src = fileReader.result as string;
+
+      async function getImageBlob(imageUrl: string) {
+        const response = await fetch(imageUrl);
+        return response.blob();
+      }
+
+      const imageBlob = await getImageBlob(fileReader.result as string);
+      imageType = imageBlob.type;
+    };
+    fileReader.readAsDataURL(image[0]);
+
+    const client = create({ url: "https://ipfs.infura.io:5001" });
+    const { cid: imageCid } = await client.add(image![0]);
 
     // ipfs hash for metadata
+    const metadata = {
+      name: title,
+      description: description,
+      attributes: [
+        {
+          key: "/venue",
+          value: venue,
+        },
+        {
+          key: "/city",
+          value: city,
+        },
+        {
+          key: "/time",
+          value: datetime,
+        },
+      ],
+      image: {
+        url: {
+          ORIGINAL: `ipfs://ipfs/${imageCid}`,
+        },
+        meta: {
+          ORIGINAL: {
+            type: imageType,
+            width: imageData.width,
+            height: imageData.height,
+          },
+        },
+      },
+    };
+
+    const { cid: metadataCid } = await client.add(JSON.stringify(metadata));
+    // this is for u @jay
+    const metadataUri = `ipfs://ipfs/${metadataCid}`;
   };
 
   useEffect(() => {
@@ -28,6 +95,14 @@ const EventCreate = () => {
     }
     getbytecode();
   }, []);
+
+  const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+
+    setImage(event.target.files);
+  };
 
   return (
     <main className="create-event">
@@ -42,6 +117,7 @@ const EventCreate = () => {
           placeholder="Event Title"
           autoFocus
           maxLength={40}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label htmlFor="description">Description</label>
         <textarea
@@ -50,6 +126,7 @@ const EventCreate = () => {
           placeholder="Event Description"
           rows={4}
           maxLength={280}
+          onChange={(e) => setDescription(e.target.value)}
         />
         <label htmlFor="venue">Venue</label>
         <input
@@ -58,6 +135,7 @@ const EventCreate = () => {
           name="venue"
           placeholder="Venue"
           maxLength={40}
+          onChange={(e) => setVenue(e.target.value)}
         />
         <label htmlFor="city">City</label>
         <input
@@ -66,9 +144,27 @@ const EventCreate = () => {
           name="city"
           placeholder="City"
           maxLength={40}
+          onChange={(e) => setCity(e.target.value)}
         />
         <label htmlFor="datetime">Date & Time</label>
-        <input type="datetime-local" id="datetime" name="datetime" />
+        <input
+          type="datetime-local"
+          id="datetime"
+          name="datetime"
+          onChange={(e) => setDatetime(e.target.value)}
+        />
+        <label htmlFor="image" className="label-show">
+          Event Image
+        </label>
+        <input
+          type="file"
+          id="image"
+          name="image"
+          maxLength={40}
+          accept=".png,.jpg,.jpeg"
+          title="Upload event image"
+          onChange={onImageChange}
+        />
         <button type="submit">Create</button>
       </form>
     </main>
