40ea3b0fec929a49ede42f79e669fea7e2ac9ed3
Mubashir Mir <mubashirmir15@gmail.com>
2022-02-19 23:27:28 -0700

    show metadata for tickets

--- src/routes/UserTickets.tsx
+++ src/routes/UserTickets.tsx
@@ -18,18 +18,21 @@ export type Ticket = {
   tokenUri: string;
   id: number;
   event?: Event;
+  type?: string,
+  description?: string,
+  image?: string,
 };
 
 const TEST_TICKETS:Ticket[] = [{
-  id: 1,
+  id: 19,
   owner: "0x42e378336c93fcF06bA92aA313F28DF7d7A2EA5d",
   tokenUri: "metadata",
 },{
-  id: 2,
+  id: 18,
   owner: "0x42e378336c93fcF06bA92aA313F28DF7d7A2EA5d",
   tokenUri: "metadata",
 },{
-  id: 3,
+  id: 17,
   owner: "0x42e378336c93fcF06bA92aA313F28DF7d7A2EA5d",
   tokenUri: "metadata",
 }]
@@ -37,7 +40,6 @@ const TEST_TICKETS:Ticket[] = [{
 const UserTickets = () => {
   const { account, library, active, chainId } = useWeb3React();
   const [tickets, setTickets] = useState<Ticket[]>([]);
-  const [tryRedeem, setRedeem] = useState(false)
   var ticketContract = new ethers.Contract(process.env.REACT_APP_TICKET_ADDRESS!, TicketABI, library)
 
   useEffect(() => {
@@ -48,10 +50,21 @@ const UserTickets = () => {
       for (const t of TEST_TICKETS) {
         try {
           const eventAddress = await ticketContract.eventAddress(t.id)
-          const tokenUri = await ticketContract.tokenURI(t.id)
+          var tokenUri = await ticketContract.tokenURI(t.id)
           const contract = new ethers.Contract(eventAddress, EventABI, library);
           const name = await contract.name();
           t.tokenUri = tokenUri;
+          if (tokenUri.startsWith("ipfs://")) {
+            tokenUri = `https://ipfs.io/ipfs/${tokenUri.split("/").pop()}`;
+          }
+          const {data} = await axios.get(tokenUri);
+          var imageUri = data.image.url.ORIGINAL;
+          if (imageUri.startsWith("ipfs://")) {
+            imageUri = `https://ipfs.io/ipfs/${imageUri.split("/").pop()}`;
+          }
+          t.description = data.description;
+          t.type = data.name;
+          t.image = imageUri;
           t.eventAddress = eventAddress;
           t.event = {
             address: eventAddress,
@@ -59,7 +72,7 @@ const UserTickets = () => {
           };
           completeTickets.push(t);
         } catch (err) {
-          return
+          continue
         }
       };
       setTickets(completeTickets);
@@ -74,16 +87,18 @@ const UserTickets = () => {
         {tickets && tickets.map((ticket, key) => {
           return (
             <React.Fragment key={key}>
-              <div className="event-hero">
-                <div className="event-description">
-                <Link to={`/event/${ticket.event!.address}`} key={key}>
-                  <h1>{ticket.event?.name}</h1>
-                </Link>
+                <div className="event-hero">
+                  <Link to={`/event/${ticket.event!.address}`} key={key}>
+                    <div className="event-description">
+                      <h1>{ticket.event?.name}</h1>
+                      <p className="description">{ticket?.description}</p>
+                      <p className="description">{ticket?.type}</p>
+                    </div>
+                  </Link>
+                  <div className="event-media">
+                    <img src={ticket?.image} alt={ticket.event?.name} />
+                  </div>
                 </div>
-                <div className="event-media">
-                  <img src={eventImage} alt={ticket.event?.name} />
-                </div>
-              </div>
               <Link to={`/user/redeem/${ticket.id}`}>
                 <button>Redeem Ticket</button>
               </Link>
